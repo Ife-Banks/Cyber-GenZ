fd9f8d3a1c1534c25b6185a681c333656c551e02
Ife-Banks <ifeoluwa.bankole05@gmail.com>
2025-05-05 23:45:57 +0100

    Link responsiveness

--- src/components/HeroSec.jsx
+++ src/components/HeroSec.jsx
@@ -72,11 +72,11 @@ const Hero = () => {
             <div className='md:block absolute -top-70 rounded-full h-[1026px] w-[1026px]  class="bg-[radial-gradient(50%_50%_at_50%_50%,rgba(22,22,24,0)_0%,rgba(22,22,24,1)_46.59%)] bg-[radial-gradient(50%_50%_at_50%_50%,color(display-p3_0.0863_0.0863_0.0941/0)_0%,color(display-p3_0.0863_0.0863_0.0941)_44.59%)] sm:max-md:bg-[radial-gradient(50%_50%_at_50%_50%,color(display-p3_0.0863_0.0863_0.0941/0)_0%,color(display-p3_0.0863_0.0863_0.0941)_50.59%)] md:bg-[radial-gradient(50%_50%_at_50%_50%,color(display-p3_0.0863_0.0863_0.0941/0)_0%,color(display-p3_0.0863_0.0863_0.0941)_60.59%)] bg-transparent' > </div>
             <Navigate/>
             {/* HEroText */}
-            <div className='z-1 absolute top-[20%]  lg:top-[22%] md:max-lg::top-[30%] text-white w-[100%] sm:max-lg:w-[90%] lg:w-[60%] flex flex-col gap-y-2 items-center'>
+            <div className='z-1 absolute top-[20%]  lg:top-[22%] md:max-lg::top-[30%] text-white w-[100%] sm:max-lg:w-[90%] lg:w-[63%] flex flex-col gap-y-5 items-center'>
                 <h1 className='font-bold  text-3xl/12 sm:max-md:text-4xl/15 md:text-5xl/18 text-center p-2 font-redHat text-shadow-sm text-shadow-white'>Empowering the Next Generation of Cyber Defenders to Protect, Innovate, and Lead in a Secure Digital World.</h1>
                 <p className='text-xs w-fit text-center text-[#6B7275] text-[color(display-p3_0.424_0.4469_0.4583)] font-inter'>Shaping Tomorrow’s Cybersecurity Leaders with Cutting-Edge Skills and Unwavering Commitment.
                 </p>
-                <div className='flex gap-x-6 mt-3 w-100 justify-center'>
+                <div className='flex gap-x-6 w-100 justify-center'>
                     <div className='py-3 px-6 rounded-3xl border border-[rgba(255,255,255,0.40)]
                     border-[color(display-p3_1_1_1/0.40)]
                     bg-[linear-gradient(180deg,_#4343F1_0%,_#00F_100%)]
