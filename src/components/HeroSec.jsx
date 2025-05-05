4d2d5d3d775ebe76c75b8990afdc1caa9a99a2fa
Ife-Banks <ifeoluwa.bankole05@gmail.com>
2025-05-05 14:07:08 +0100

    Text added

--- src/components/HeroSec.jsx
+++ src/components/HeroSec.jsx
@@ -58,20 +58,23 @@ const Hero = () => {
                 <div className='border-1 border-t-0  border-white/3 h-19 w-18 bg-[rgba(255,255,255,0.30)]'></div>
                 <div className='border-l-0 border-t-0  border border-[rgba(255,255,255,0.30)] h-19 w-18 bg-[#131718]'></div>
                 <div className='border-l-0 border-t-0   border border-[rgba(255,255,255,0.30)] h-19 w-18 bg-[#131718]'></div>
-                <div className='border-l-0 border-t-0  border border-[rgba(255,255,255,0.30)] h-19 w-18 bg-[#131718]'></div>
-                <div className='border-l-0 border-t-0  border border-[rgba(255,255,255,0.30)] h-19 w-18 bg-[#131718]'></div>
-                <div className='border-l-0 border-t-0  border border-[rgba(255,255,255,0.30)] h-19 w-18 bg-[#131718]'></div>
-                <div className='border-l-0 border-t-0  border border-[rgba(255,255,255,0.30)] h-19 w-18 bg-[#131718]'></div>
-                <div className='border-l-0 border-t-0  border border-[rgba(255,255,255,0.30)] h-19 w-18 bg-[#131718]'></div>
-                <div className='border-l-0 border-t-0  border border-[rgba(255,255,255,0.30)] h-19 w-18 bg-[#131718]'></div>
                 <div className='hidden sm:block border-l-0 border-t-0  border border-[rgba(255,255,255,0.30)] h-19 w-18 bg-[#131718]'></div>
                 <div className='hidden sm:block border-l-0 border-t-0  border border-[rgba(255,255,255,0.30)] h-19 w-18 bg-[#131718]'></div>
                 <div className='hidden sm:block border-l-0 border-t-0  border border-[rgba(255,255,255,0.30)] h-19 w-18 bg-[#131718]'></div>
                 <div className='hidden sm:block border-l-0 border-t-0  border border-[rgba(255,255,255,0.30)] h-19 w-18 bg-[#131718]'></div>
-               
+                <div className='hidden sm:block border-l-0 border-t-0  border border-[rgba(255,255,255,0.30)] h-19 w-18 bg-[#131718]'></div>
+                <div className='hidden sm:block border-l-0 border-t-0  border border-[rgba(255,255,255,0.30)] h-19 w-18 bg-[#131718]'></div>
+                <div className='hidden sm:block border-l-0 border-t-0  border border-[rgba(255,255,255,0.30)] h-19 w-18 bg-[#131718]'></div>
+                <div className='hidden sm:block border-l-0 border-t-0  border border-[rgba(255,255,255,0.30)] h-19 w-18 bg-[#131718]'></div>
+                <div className='hidden sm:block border-l-0 border-t-0  border border-[rgba(255,255,255,0.30)] h-19 w-18 bg-[#131718]'></div>
+                <div className='hidden sm:block border-l-0 border-t-0  border border-[rgba(255,255,255,0.30)] h-19 w-18 bg-[#131718]'></div>
             </div> 
             <div className='md:block absolute -top-70 rounded-full h-[1026px] w-[1026px]  class="bg-[radial-gradient(50%_50%_at_50%_50%,rgba(22,22,24,0)_0%,rgba(22,22,24,1)_46.59%)] bg-[radial-gradient(50%_50%_at_50%_50%,color(display-p3_0.0863_0.0863_0.0941/0)_0%,color(display-p3_0.0863_0.0863_0.0941)_44.59%)] sm:max-md:bg-[radial-gradient(50%_50%_at_50%_50%,color(display-p3_0.0863_0.0863_0.0941/0)_0%,color(display-p3_0.0863_0.0863_0.0941)_50.59%)] md:bg-[radial-gradient(50%_50%_at_50%_50%,color(display-p3_0.0863_0.0863_0.0941/0)_0%,color(display-p3_0.0863_0.0863_0.0941)_60.59%)] bg-transparent' > </div>
             <Navigate/>
+            {/* HEroText */}
+            <div className='z-1 absolute top-[20%]  lg:top-[25%] md:max-lg::top-[30%] text-white w-[100%] sm:max-lg:w-[90%] lg:w-[75%] font-bold  text-3xl/12 sm:max-md:text-4xl/15 md:text-5xl/16 text-center p-2 font-redHat text-shadow-sm text-shadow-white'>
+                <h1>Empowering the Next Generation of Cyber Defenders to Protect, Innovate, and Lead in a Secure Digital World.</h1>
+            </div>
         </div>
        
      );
